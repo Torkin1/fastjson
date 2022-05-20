bd717302d05d4b1a2ebd3ce30f2e97dbed276a9c
Torkin <daniele.laprova@hotmail.it>
2022-05-20 18:59:22 +0200

    #7

--- generateBaDuaReport.sh
+++ generateBaDuaReport.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-MVN_LOCAL_REPO=$(mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
+# $1: path to compiled test classes to instrument
+# $2: where to save report
 
-java -jar "$MVN_LOCAL_REPO/br/usp/each/saeg/ba-dua-cli/0.6.0/ba-dua-cli-0.6.0-all.jar" report -classes target/test-classes/ -xml target/report.xml -input target/badua.ser -show-classes -show-methods
+MVN_LOCAL_REPO=$(mvn help:evaluate -Dexpression=settings.localRepository -q -DforceStdout)
+BADUA_CLI_PATH="$MVN_LOCAL_REPO/br/usp/each/saeg/ba-dua-cli/0.6.0/ba-dua-cli-0.6.0-all.jar"
+
+java -jar "$BADUA_CLI_PATH" report -classes "$1" -xml "$2" -input target/badua.ser -show-classes -show-methods
--- installBaDua.sh
+++ installBaDua.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
+# $1: ba-dua release
+
 WORKDIR=$(mktemp -d)
-BADUA_RELEASE="ba-dua-0.6.0"
+BADUA_RELEASE="ba-dua-$1"
 BADUA_FROM="https://github.com/saeg/ba-dua/archive/refs/tags/$BADUA_RELEASE.zip"
 toUnzip="$BADUA_RELEASE.zip"
 
 cd "$WORKDIR" || exit 
-wget $BADUA_FROM -O $toUnzip
-unzip $toUnzip
+wget "$BADUA_FROM" -O "$toUnzip"
+unzip "$toUnzip"
 cd "ba-dua-$BADUA_RELEASE" || exit
 mvn clean install
