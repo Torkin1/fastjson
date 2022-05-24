42bf91cc8e66b4127ffd3550fd0ae35f7313ac3e
Torkin <daniele.laprova@hotmail.it>
2022-05-24 23:50:46 +0200

    #8 - cleaned commented lines and unused imports

--- src/test/java/com/alibaba/json/bvt/serializer/MapTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/MapTest.java
@@ -2,18 +2,10 @@ package com.alibaba.json.bvt.serializer;
 
 import com.alibaba.fastjson.annotation.JSONField;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TestName;
-import org.junit.runner.OrderWith;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-
-import clojure.lang.Obj;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
@@ -24,8 +16,6 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import static org.junit.Assert.assertEquals;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.EnumSet;
@@ -36,37 +26,10 @@ import java.util.Set;
 @RunWith(Parameterized.class)
 public class MapTest {
 
-    /*private enum MapTests{
-        TEST_NO_SORT("test_no_sort"),
-        TEST_NULL("test_null"),
-        TEST_ON_JSON_FIELD("test_onJSONField");
-
-        public String testName;
-
-        MapTests(String testName){
-            this.testName = testName;
-        }
-
-        public static MapTests getTestFromName(String name){
-            for (MapTests t : MapTests.values()){
-                if (t.testName.equals(name)){
-                    return t;
-                }
-            }
-            return null;
-        }
-
-
-    }
-    */
-    //private JSONObject obj;
-    //private MapNullValue mapNullValue;
     private final Object holder;
     private final Set<SerializerFeature> features;
     private final String expected;
     private final Class<?> jsonParser;
-   // @Rule
-   // public TestName testName = new TestName(); 
     
    public MapTest(String expected, Object holder, Set<SerializerFeature> features, Class<?> jsonParser, Map<String, Object>... couples) throws Exception {
        this.expected = expected;
@@ -128,60 +91,7 @@ public class MapTest {
         }
         assertEquals(expected, actual);
     }
-     /*   
-    @Before
-    public void configure(){
-        MapNullValue mapNullValue = new MapNullValue();
-        mapNullValue.setMap(new HashMap<String,Object>());
-        JSONObject obj = new JSONObject();
-        String currentTestName =testName.getMethodName();
-        MapTests currentTest = MapTests.getTestFromName(currentTestName);
 
-        switch(currentTest){
-
-            case TEST_NO_SORT:
-                obj.put("name", "jobs");
-                obj.put("id", 33);
-                this.expected = "{'name':'jobs','id':33}";
-                break;
-            case TEST_NULL:
-                obj.put("name", null);
-                this.expected = "{\"name\":null}";
-                break;
-
-            case TEST_ON_JSON_FIELD:
-                mapNullValue.getMap().put("Ariston", null);
-                this.expected = "{\"map\":{\"Ariston\":null}}";
-                break;
-            }
-        this.obj = obj;
-        this.mapNullValue = mapNullValue;
-
-    }
-
-    
-    @After
-    public void clean(){
-        this.obj = null;
-        this.mapNullValue = null;
-        this.expected = null;
-
-    }
-    
-    @Test
-    public void test_no_sort() throws Exception {
-        String text = toJSONString(obj);
-        Assert.assertEquals(expected, text);
-    }
-    
-    @Test
-
-    public void test_null() throws Exception {
-        
-        String text = JSON.toJSONString(obj, SerializerFeature.WriteMapNullValue);
-        Assert.assertEquals(expected, text);
-    }
-*/
     public static final String toJSONString(Object object) {
         SerializeWriter out = new SerializeWriter();
 
@@ -199,13 +109,7 @@ public class MapTest {
             out.close();
         }
     }
-/*
-    @Test
-    public void test_onJSONField() {
-        String json = JSON.toJSONString( mapNullValue );
-        Assert.assertEquals(expected, json);
-    }
-*/
+
     static class MapNullValue {
         @JSONField(serialzeFeatures = {SerializerFeature.WriteMapNullValue})
         private Map map;
